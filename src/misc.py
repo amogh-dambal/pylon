643116b709994d1ae1c25b2b64dcce94a1e5ac51
amogh <amdam2013@gmail.com>
2021-07-04 18:41:54 -0500

    finish implementing all basic nflfastR functions

--- src/misc.py
+++ src/misc.py
@@ -1,13 +1,12 @@
 # python 3.6
 # file for the nflfastR functions I couldn't find a place for
 import pandas as pd
-import rpy2.robjects as robjects
-from rpy2.robjects import pandas2ri
+import pyreadr
 
-from src.util.urls import ROSTER_URL, SCHEDULE_URL, STATS_URL
+from src.util.urls import ROSTER_URL, STATS_URL
 
 
-# TODO: might need to look into the Sleeper API to get roster data. For now,
+# TODO: look into the Sleeper API to get roster data. For now,
 # 		we just use the repo that has the data already stored
 def get_roster(seasons: list) -> pd.DataFrame:
 	"""
@@ -20,21 +19,27 @@ def get_roster(seasons: list) -> pd.DataFrame:
 		for season in seasons
 		if 1999 <= int(season) <= 2020
 	]
-	df = pd.concat(roster_dfs)
+	df = pd.concat(roster_dfs).reset_index(drop=True, inplace=True)
 	return df
 
 
-def get_schedules(seasons: list) -> pd.DataFrame:
+def get_schedules(seasons: list, filepath: str = '../data/schedules/sched_{season}.rds') -> pd.DataFrame:
 	# https://stackoverflow.com/questions/64178038/how-to-read-a-rds-file-from-a-url-in-python
+	# credit to ofajardo for developing the pyreadr package
+	schedule_dfs = [
+		pyreadr.read_r(filepath.format(season=season))[None]
+		for season in seasons
+		if 1999 <= int(season) <= 2020
+	]
 
-	# TODO: come back to this later when I can figure out RDS files RIP LMAO
-	return None
+	df = pd.concat(schedule_dfs).reset_index(drop=True, inplace=True)
+	return df
 
 
 def get_stats(serialize: bool = False) -> pd.DataFrame:
 	"""
 	function that obtains player statistics
-	:param serialize: if this is True, use serialization for greater efficiency(not supported yet)
+	:param serialize: if this is True, use serialization for greater efficiency (not supported yet)
 	:return: pandas DataFrame object with all NFL player stats as maintained by nflfastR.
 	"""
 	df = pd.read_csv(STATS_URL, compression='gzip', low_memory=False)
--- src/pbp/functions.py
+++ src/pbp/functions.py
@@ -2,6 +2,7 @@
 # wrappers around nflfastR functions
 # that return pandas DataFrames
 import pandas as pd
+from ..util.urls import PBP_URL
 
 
 def build_nflfastr_pbp(game_ids: list, decode: bool = True, rules: bool = True) -> pd.DataFrame:
@@ -9,4 +10,11 @@ def build_nflfastr_pbp(game_ids: list, decode: bool = True, rules: bool = True) 
 
 
 def load_pbp(seasons: list, serialize: bool = True) -> pd.DataFrame:
-	pass
+	pbp_dfs = [
+		pd.read_csv(PBP_URL.format(season=season), error_bad_lines=False)
+		for season in seasons
+		if 1999 <= int(season) <= 2020
+	]
+
+	df = pd.concat(pbp_dfs).reset_index(drop=True, inplace=True)
+	return df
