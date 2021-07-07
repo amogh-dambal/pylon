d38d0e7b0da6e1da7ee2a3e16a62825f81e82231
amogh <amdam2013@gmail.com>
2021-07-07 01:42:58 -0500

    added new files for basic analysis/viz features.

---
+++ src/analyzer.py
@@ -0,0 +1,11 @@
+# python 3.6
+# basic data analysis functions
+import pandas as pd
+
+
+def filter(pbp: pd.DataFrame, **kwargs: dict) -> pd.DataFrame:
+	pass
+
+
+def query(pbp: pd.DataFrame, **kwargs: dict) -> pd.DataFrame:
+	pass
