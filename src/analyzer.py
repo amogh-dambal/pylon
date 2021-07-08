21e993bb06cc0611f0c045c882610b5b2b1e9ef2
amogh <amdam2013@gmail.com>
2021-07-08 00:03:09 -0500

    implement get_performance method - should be bread & butter of analytics

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,6 +1,7 @@
 # python 3.6
 # basic data analysis functions
 import pandas as pd
+from copy import deepcopy
 
 
 def filter(pbp: pd.DataFrame, **kwargs: dict) -> pd.DataFrame:
@@ -44,3 +45,63 @@ def find_game(pbp: pd.DataFrame, **kwargs: dict) -> pd.DataFrame:
 	return df
 
 
+def get_performance(pbp: pd.DataFrame, pos: str, **kwargs) -> pd.DataFrame:
+	"""
+	function that returns season-wide statistics for players at a particular position
+	can specify metrics in kwargs
+	:param pbp: play-by-play dataset
+	:param pos: position to query for. must be one of
+	- QB
+	- RB
+	- WR
+	- TE
+	:param kwargs: additional parameters can be included in the query. currently only supports EPA.
+	- 	agg: aggregation data you can pass to the function
+	- 	min_plays: minimum number of plays for inclusion into analysis. default is 200.
+	- 	sort: will sort the dataframe before returning. default is true.
+	- 	sort_direction: must only be present if sort is present.
+		Set this to true if you want to sort in ascending order, false otherwise. default is false.
+	- 	sort_by: must only be present if sort is present. column on which to sort each record. default is EPA
+	- 	round: precision to which dataframe will round values. default is 3.
+	:return: filtered and sorted dataframe containing results of player performance
+	"""
+	# TODO: write a subroutine to validate kwargs
+	ops = {
+		'epa': 'mean',
+		'cpoe': 'mean',
+		'play_id': 'count'
+	}
+	if 'agg' in kwargs:
+		ops = deepcopy(kwargs['agg'])
+
+	players = pbp.groupby(
+		[pos, 'posteam'],
+		as_index=False
+	).agg(ops)
+
+	# we want to establish a certain threshold
+	# so that we only include meaningful data in the
+	# analysis.
+	if 'min_plays' in kwargs:
+		mp = kwargs['min_plays']
+	else:
+		mp = 200
+	players = players.loc[players.play_id >= mp]
+
+	# have to configure the sorting options so its invalid to specify sort properties
+	# without first specifying that they want to sort.
+	if 'sort' not in kwargs and ('sort_descending' in kwargs or 'sort_by' in kwargs):
+		raise ValueError("Error! Trying to specify sorting behavior without specifying sort.")
+	sort_options = {
+		'sort': True if 'sort' not in kwargs else kwargs['sort'],
+		'sort_ascending': False if 'sort_dir' not in kwargs else kwargs['sort_dir'],
+		'sort_by': 'epa' if 'sort_by' not in kwargs else kwargs['sort_by']
+	}
+	if sort_options['sort']:
+		players.sort_values(sort_options['sort_by'], ascending=sort_options['sort_ascending'], inplace=True)
+
+	# 3 just seems like a good number. Deryck97 used 2, but I like 3 better as a number.
+	precision = 3 if 'round' not in kwargs else kwargs['round']
+	players = players.round(precision)
+
+	return players
