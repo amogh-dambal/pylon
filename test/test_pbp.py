bba38e3ca2a264005c9318de72122d284dfaa855
amogh <amdam2013@gmail.com>
2021-07-15 19:41:41 -0500

    get ready for build and distribution to pypi

--- test/test_pbp.py
+++ test/test_pbp.py
@@ -11,6 +11,8 @@ class TestPBP(unittest.TestCase):
 		expected_shape = (38151, 372)
 		self.assertEqual(actual_shape, expected_shape)
 
+	# TODO: write more unit tests for test_load. have to play around with the data to model expected behavior
+	# 		so we can actually write the tests
 
 if __name__ == '__main__':
 	unittest.main()
